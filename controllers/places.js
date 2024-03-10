1312e6f63a74cf24eb618f4d56be4df628f64f40
Omar <o.moreta@gmail.com>
2024-03-10 03:43:09 -0400

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,25 +1,27 @@
 const router = require('express').Router()
+const places = require("../models/places.js")
 
 // GET /places - places index
 router.get("/", (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/404-cat.jpg'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/dessert-cups.jpg'
-    }]
-
     res.render("places/index", { places })
 })
 
 // POST /places - create places
+router.post('/', (req, res) => {
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+})
+
 // GET /places/new - form to create places
 router.get("/new", (req, res) => {
     res.render("places/new")
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.set("views", __dirname + "/views")
 app.set("view engine", "jsx")
 app.engine("jsx", require("express-react-views").createEngine())
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
 
 // GET /places/* - renders views/functionality in controllers
 app.use("/places", require("./controllers/places"))
