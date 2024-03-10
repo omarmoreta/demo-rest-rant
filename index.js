9ca34ec1c2d11810b17e243d88409ddb2a8887bc
Omar <o.moreta@gmail.com>
2024-03-10 01:29:37 -0500

    adding places controller

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT
+
+app.use("/places", require("./controllers/places"))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
