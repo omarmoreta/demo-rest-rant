788ef66603788e00fb3292d005fc489a68b5ab4e
Omar <o.moreta@gmail.com>
2024-03-10 03:08:42 -0400

    adding bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -7,13 +7,13 @@ router.get("/", (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/404-cat.jpg'
     }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/dessert-cups.jpg'
     }]
 
     res.render("places/index", { places })
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,17 +4,26 @@ const Def = require('../default')
 function Index(data) {
     let placesFormatted = data.places.map((place, i) => {
         return (
-            <div key={i}>
+            <div key={i} className="col-sm-6">
                 <h2>{place.name}</h2>
+                <p className="text-center">
+                    {place.cuisines}
+                </p>
                 <img src={place.pic} alt={place.name} />
+                <p className="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     })
+
     return (
         <Def>
             <main>
                 <h1>PLACES INDEX PAGE</h1>
-                {placesFormatted}
+                <div className="row">
+                    {placesFormatted}
+                </div>
             </main>
         </Def>
     )
