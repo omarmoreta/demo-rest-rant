792d677a9547449249d475cee4bb3b920954361d
Omar <o.moreta@gmail.com>
2024-03-10 01:47:39 -0500

    adding 404 page view

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.use("/places", require("./controllers/places"))
 
 // GET / - home page
 app.get('/', (req, res) => {
-    res.render("home")
+    res.status(200).render("home")
 })
 
 // GET /* - error page
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).render("error404")
 })
 
 // server listening on PORT variable defined in .env
